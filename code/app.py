2e10b3ceffce18b82493e735ff4350b8f0a91f0c
Bertrand Steinmetz <bstenm@hotmail.com>
2019-08-29 16:07:40 -0400

    adding jwt authentication

--- code/app.py
+++ code/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+import credentials
 
 app = Flask(__name__)
-
+app.secret_key = credentials.jwt_key
 api = Api(app)
+
+# create a new endpoint "/auth"
+jwt = JWT(app, authenticate, identity)
 
 stores = []
 
 
 class Store(Resource):
+
+    @jwt_required()
     def get(self, name):
         matches = filter(lambda store: store["name"] == name, stores)
         store = next(matches, None)
