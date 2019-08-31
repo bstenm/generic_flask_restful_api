d8b4515a621c3a58b0759b864037164221a5cad0
Bertrand Steinmetz <bstenm@hotmail.com>
2019-08-31 16:54:22 -0400

    persisting users into sqlite

--- code/app.py
+++ code/app.py
@@ -1,40 +1,58 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
+from user import UserRegister
 import credentials
 
 app = Flask(__name__)
 app.secret_key = credentials.jwt_key
 api = Api(app)
 
-# create a new endpoint "/auth"
+# create a new endpoint '/auth'
 jwt = JWT(app, authenticate, identity)
 
 stores = []
 
 
 class Store(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('feature1',
+                        type=str,
+                        required=True,
+                        help="Can not be left empty"
+                        )
 
     @jwt_required()
     def get(self, name):
-        matches = filter(lambda store: store["name"] == name, stores)
+        matches = filter(lambda store: store['name'] == name, stores)
         store = next(matches, None)
-        return {"store": store}
+        return {'store': store}
 
     def post(self, name):
-        matches = filter(lambda store: store["name"] == name, stores)
+        data = Store.parser.parse_args()
+        matches = filter(lambda store: store['name'] == name, stores)
         if next(matches, None):
-            return {"message": f"A store with name '{name}' already exists"}, 400
+            return {'message': f'A store with name "{name}" already exists'}, 400
 
-        data = request.get_json()
-        store = {"name": name, "feature1": data["feature1"]}
+        store = {'name': name, 'feature1': data['feature1']}
         stores.append(store)
         return store, 201
 
+    def put(self, name):
+        data = Store.parser.parse_args()
+        matches = filter(lambda store: store['name'] == name, stores)
+        store = next(matches, None)
+        if store is None:
+            store = {'name': name, 'feature1': data['feature1']}
+            stores.append(store)
+        else:
+            store.update(data)
+        return store
+
     def delete(self, name):
         global stores
-        stores = list(filter(lambda x: x["name"] != name, stores))
+        stores = list(filter(lambda x: x['name'] != name, stores))
 
 
 class StoreList(Resource):
@@ -42,7 +60,8 @@ class StoreList(Resource):
         return stores
 
 
-api.add_resource(Store, "/store/<string:name>")
-api.add_resource(StoreList, "/stores")
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
--- code/user.py
+++ code/user.py
@@ -1,5 +1,68 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.password = password
         self.username = username
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        # execute query passing a tuple
+        query = "SELECT * from users where username=?"
+        cursor.execute(query, (username,))
+        row = cursor.fetchone()
+        connection.close()
+
+        return cls(*row) if row else None
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        # execute query passing a tuple
+        query = "SELECT * from users where id=?"
+        cursor.execute(query, (_id,))
+        row = cursor.fetchone()
+        connection.close()
+
+        return cls(*row) if row else None
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="A username should be provided"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="A password should be provided"
+                        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        password = data['password']
+        username = data['username']
+
+        if User.find_by_username(username):
+            return {'message': 'A user with that username already exists'}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES(NULL, ?, ?)"
+        # TODO: encrypt password
+        cursor.execute(query, (username, password))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'User successfully created'}, 201
